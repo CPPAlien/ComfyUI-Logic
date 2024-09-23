6ce260a5a868c87d89abb591425c07aba7f948a1
CPPAlien <creaspan@gmail.com>
2024-09-23 19:17:57 +0800

    feat: compatible with old version

--- nodes.py
+++ nodes.py
@@ -240,6 +240,7 @@ NODE_CLASS_MAPPINGS = {
     "String-🔬": String,
     "If ANY return A else B-🔬": IfExecute,
     "DebugPrint-🔬": DebugPrint,
+    "String": String,
     "If ANY execute A else B": IfExecute,
     # "If ANY execute A else B-🔬": IfExecuteNode,
 }
@@ -253,6 +254,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "String-🔬": "String",
     "If ANY return A else B-🔬": "If ANY return A else B",
     "DebugPrint-🔬": "DebugPrint",
+    "String": "String",
     "If ANY execute A else B": "If",
     # "If ANY execute A else B-🔬": "If ANY execute A else B",
 }
